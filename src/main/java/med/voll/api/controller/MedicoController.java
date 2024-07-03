03441b8bd2cc371327c75541aff3824d63f0dad3
Gabriel Aquino <gabrielaquino.tec@gmail.com>
2024-07-03 00:28:24 -0300

    feat: add medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,13 +1,14 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -16,7 +17,27 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados ){
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados ){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    };
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizarMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizar(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
